2aa92b1618cd953cfbcc8b7ded8b92da0a4d426b
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-03-09 16:50:23 -0600

    Mejorando el crud de categorias
    Lo borre ya que pienso que primero debo de crear los productos y despues las categorias

--- src/models/categoria.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const categoriSchema = Schema({
-    nombre: String
-})
-
-module.exports = mongoose.model('Categorias', categoriSchema)

b652493cf75c5ba4170f2596c2539ea43d1f99a9
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-03-11 20:14:48 -0600

    Empezar Facturas
    Lo deje hasta aqui por problemas de luz, y esto lo subi con datos

--- src/controllers/factura.controllers.js
+++ src/controllers/factura.controllers.js
@@ -1,9 +1,18 @@
-const Factura = require('../models/factura.model')
-const Carrito = require('../models/carritoCompras.model')
-const Productos = require('../models/producto.model')
+const Factura = require("../models/factura.model");
+const Carrito = require("../models/carritoCompras.model");
+const Productos = require("../models/producto.model");
 
-function confirmarCompra (req, res) {
+function confirmarCompra(req, res) {
+  const usuarioId = req.user.sub;
 
-    
+  Carrito.findOne({ usuario: usuarioId }, (err, buscarProducto) => {
+    if (buscarProducto == null)
+      return res.status(500).send({ mensaje: "No tienes productos agregados" });
+    {
+        for(let carrito of buscarProducto.productos) {
+            
+        }
+    }
 
+  }).lean();
 }
